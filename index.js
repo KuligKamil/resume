b15f698bdc9447ad805beb5a552aaa973706fc4a
KuligKamil <kamil.marek.kulig@gmail.com>
2023-07-29 23:32:23 +0200

    🚀 chore: first version of resume from webpage to pdf

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const process = require('node:process')
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  const website_url = process.env.FILE_DIRECTION
+
+  await page.goto(website_url, { waitUntil: 'networkidle0' })
+
+  await page.emulateMediaType('screen')
+
+  await page.pdf({
+    path: 'dist/Kamil Kulig resume.pdf',
+    // margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+    printBackground: true,
+    format: 'A4',
+  })
+  await browser.close()
+})()
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Resume
+
+## Config
+
+  create and add .env file with 
+  
+    FILE_DIRECTION=""
+
+  install project
+
+    pnpm i
+
+## Run 
+
+    node index.js && open 'dist/Kamil Kulig resume.pdf'
