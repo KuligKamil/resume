b4cff95a7939902e1beb98e497346c45afa9af33
KuligKamil <kamil.marek.kulig@gmail.com>
2023-10-02 08:47:25 +0200

    chore: add resume

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
 const process = require('node:process')
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+require('dotenv').config();
 
 (async () => {
   const browser = await puppeteer.launch()
